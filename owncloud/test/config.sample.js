c9a4ae16c799a3969dd80aff0c6359e4da142f82
noveens <noveen.sachdeva@research.iiit.ac.in>
2017-05-16 03:48:06 +0530

    travis updated

--- owncloud/test/config.sample.js
+++ owncloud/test/config.sample.js
@@ -1,23 +1,25 @@
 // unique id to use for testing
-var testId = new Date.getTime();
+var testId = new Date().getTime();
 
-var Config = {
+var config = {
     // Change this to your ownCloud's URL
-    'owncloudURL': 'http://127.0.0.1/',
+    'owncloudURL': 'noveen.tk/core',
     // ownCloud login
-    'owncloudUsername': 'admin',
+    'username': 'testUser',
     // ownCloud password
-    'owncloudPassword': 'admin',
+    'password': 'testPassword',
     // test user whom we want to share a file
-    'owncloudShare2user': 'share',
-    // test group, uses to add owncloudShare2user to it etc
+    'owncloudShare2user': 'owncloud',
+    // test group, uses to add owncloud_share2user to it etc
     'testGroup': 'testGroup',
     // remote root path to use for testing
     'testRoot': 'someRoot' + testId,
     // app name to use when testing privatedata API
     'appName': 'someAppName' + testId,
-    // groups to be created
+    //groups to be created
     'groupsToCreate': ["group1","group2","group3"],
-    // not existing group
+    //not existing group
     'notExistingGroup': 'someRandomGroupNameWhichDoesntExist'
 };
+
+module.exports = config;

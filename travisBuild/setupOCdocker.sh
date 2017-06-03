d35595968e76e570d97e4e3657435cc12b7382d7
noveens <noveen.sachdeva@research.iiit.ac.in>
2017-06-03 14:52:10 +0530

    sleep time increased for OCC

--- travisBuild/setupOCdocker.sh
+++ travisBuild/setupOCdocker.sh
@@ -5,6 +5,6 @@ docker pull owncloud:$OC_VERSION
 DOCKER_ID=$(docker run -d -p 80:80 owncloud:$OC_VERSION)
 
 # needed else occ isn't available directly...
-sleep 5
+sleep 10
 
 docker exec -u www-data $DOCKER_ID ./occ maintenance:install --admin-user="admin" --admin-pass="admin" --database="sqlite"

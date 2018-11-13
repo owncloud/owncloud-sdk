c539f127d5f795240c330ae77657b297e7ebbcb6
Carsten Schildknecht <carsten.schildknecht@serv3r.de>
2018-11-13 02:30:28 +0100

    made example working with cypress

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,33 +1,45 @@
 import ownCloud from '../../owncloud/owncloud'
-import config from '../../owncloud/test/config'
+import config from '../../owncloud/test/config.json'
 
 describe('My First Test', function () {
 
+    var timeRightNow = new Date().getTime();
     var owncloudURL = config.owncloudURL
-    let username = "admin"
-    let password = "admin"
-
+    let username = config.username
+    let password = config.password
+    var testFolder = '/testFolder'
 // LIBRARY INSTANCE
     var oc;
+    let ocLogin;
+
 
     beforeEach(function (done) {
         oc = new ownCloud(config.owncloudURL);
-        oc.login(config.username, config.password).then(status => {
-            expect(status).toEqual({id: 'admin', 'display-name': 'admin', email: {}});
-            done();
-        }).catch(error => {
+        ocLogin = oc.login(config.username, config.password).then(res => {
+          cy.wrap({id: res.id}).its('id').should('eq', config.username);
+          cy.wrap({'display-name': res['display-name']}).its('display-name').should('eq', config.username);
+          cy.wrap({email: res.email}).its('email').should('eq', config.email);
+          done();
+        })
+        .catch(error => {
             expect(error).toBe(null);
             done();
         });
     });
 
+
     it('checking method : list with 2 depth', function (done) {
         oc.files.list(testFolder, 2).then(files => {
-            expect(typeof(files)).toBe('object');
-            expect(files.length).toEqual(7);
-            expect(files[3].getName()).toEqual('subdir');
-            expect(files[4].getPath()).toEqual(testFolder + '/' + 'subdir/');
-            done();
+          cy.wrap(typeof(files)).should('eq', 'object');
+          cy.wrap(files).its('length').should('eq', 3)
+          cy.wrap(files[0]).its('type').should('eq', 'folder');
+          cy.wrap(files[1]).invoke('getName').then(res => {
+            cy.wrap(res).should('eq', 'subdir');
+          })
+          cy.wrap(files[2]).invoke('getPath').then(res => {
+            cy.wrap(res).should('eq', testFolder + '/' + 'subdir/')
+          })
+          done();
         }).catch(error => {
             expect(error).toBe(null);
             done();

5978dd5cfd17b2355d9a68d16cf94bbca5db8744
Thomas Müller <thomas.mueller@tmit.eu>
2020-04-03 10:54:50 +0200

    Reducing package size by using webpack-node-externals

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const nodeExternals = require('webpack-node-externals');
+
 module.exports = {
   mode: 'production',
   entry: {
@@ -11,6 +13,8 @@ module.exports = {
     libraryTarget: 'umd',
     globalObject: 'this'
   },
+  target: 'node', // in order to ignore built-in modules like path, fs, etc.
+  externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
   module: {
     rules: [{
       test: /\.js?$/,

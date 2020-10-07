e8e751da92720cde452b9c96d2c4c6f5afed7fa2
Parajuli Kiran <kiranparajuli589@gmail.com>
2020-10-07 16:34:49 +0545

    Refactored unauthorized app test

--- tests/unauthorized/appsTest.js
+++ tests/unauthorized/appsTest.js
@@ -1,19 +1,65 @@
 fdescribe('Unauthorized: Currently testing apps management,', function () {
   // CURRENT TIME
-  var timeRightNow = new Date().getTime()
-  var OwnCloud = require('../../src')
-  var config = require('../config/config.json')
+  const timeRightNow = new Date().getTime()
+  const OwnCloud = require('../../src')
+  const config = require('../config/config.json')
 
   // LIBRARY INSTANCE
-  var oc
+  let oc
 
+  // TEST setup
+  const key = ['attr1', 'attr%2Bplus%20space', '%C3%A5%C2%B1%C2%9E%C3%A6%C2%80%C2%A71']
+  const value = ['value1', 'value%2Bplus+space+and%2Fslash', '%C3%A5%C2%80%C2%BC%C3%A5%C2%AF%C2%B91']
   // PACT setup
   const Pact = require('@pact-foundation/pact-web')
   const provider = new Pact.PactWeb()
-  const { setGeneralInteractions } = require('../pactHelper.js')
-
+  const { setGeneralInteractions, invalidAuthHeader, unauthorizedXmlResponseBody } = require('../pactHelper.js')
+  const unauthorizedResponseObject = {
+    status: 401,
+    headers: {
+      'Content-Type': 'text/xml; charset=utf-8',
+      'Access-Control-Allow-Origin': origin
+    },
+    body: unauthorizedXmlResponseBody
+  }
+  const invalidAuthHeaderObject = {
+    authorization: invalidAuthHeader,
+    Origin: origin
+  }
   beforeAll(function (done) {
-    Promise.all(setGeneralInteractions(provider)).then(done, done.fail)
+    const promises = []
+    promises.push(setGeneralInteractions(provider))
+    const requiredAppMethods = ['GET', 'POST', 'DELETE']
+    requiredAppMethods.forEach((method) => {
+      promises.push(provider.addInteraction({
+        uponReceiving: `an ${method} app request with invalid auth`,
+        withRequest: {
+          method: method,
+          path: Pact.Matchers.regex({
+            matcher: '.*\\/ocs\\/v(1|2)\\.php\\/cloud\\/apps.*',
+            generate: '/ocs/v1.php/cloud/apps'
+          }),
+          headers: invalidAuthHeaderObject
+        },
+        willRespondWith: unauthorizedResponseObject
+      }))
+    })
+    const requiredAttributesMethodsArray = ['GET', 'POST']
+    requiredAttributesMethodsArray.forEach((method) => {
+      promises.push(provider.addInteraction({
+        uponReceiving: `a ${method} request for attributes with invalid authorization`,
+        withRequest: {
+          method: method,
+          path: Pact.Matchers.regex({
+            matcher: '.*\\/ocs\\/v(1|2)\\.php\\/privatedata\\/.*',
+            generate: '/ocs/v1.php/privatedata/attribute/' + config.testApp
+          }),
+          headers: invalidAuthHeaderObject
+        },
+        willRespondWith: unauthorizedResponseObject
+      }))
+    })
+    Promise.all(promises).then(done, done.fail)
   })
 
   afterAll(function (done) {
@@ -45,11 +91,9 @@ fdescribe('Unauthorized: Currently testing apps management,', function () {
   })
 
   it('checking method : setAttribute', function (done) {
-    var key = ['attr1', 'attr+plus space', '属性1']
-    var value = ['value1', 'value+plus space and/slash', '值对1']
-    var count = 0
+    let count = 0
 
-    for (var i = 0; i < key.length; i++) {
+    for (let i = 0; i < key.length; i++) {
       oc.apps.setAttribute(config.testApp, key[i], value[i]).then(status => {
         expect(status).toBe(null)
         done()
@@ -64,10 +108,9 @@ fdescribe('Unauthorized: Currently testing apps management,', function () {
   })
 
   it('checking method : getAttribute', function (done) {
-    var key = ['attr1', 'attr+plus space', '属性1']
-    var count = 0
+    let count = 0
 
-    for (var i = 0; i < key.length; i++) {
+    for (let i = 0; i < key.length; i++) {
       oc.apps.getAttribute(config.testApp, key[i]).then(data => {
         expect(data).toBe(null)
         done()
@@ -82,10 +125,9 @@ fdescribe('Unauthorized: Currently testing apps management,', function () {
   })
 
   it('checking method : deleteAttribute', function (done) {
-    var key = ['attr1', 'attr+plus space', '属性1']
-    var count = 0
+    let count = 0
 
-    for (var i = 0; i < key.length; i++) {
+    for (let i = 0; i < key.length; i++) {
       oc.apps.deleteAttribute(config.testApp, key[i]).then(status => {
         expect(status).toBe(null)
         done()

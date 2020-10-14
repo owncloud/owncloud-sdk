fb78fa728a13539acea7d0e204d85b1b2a49b0bd
swoichha <swoichhaa@gmail.com>
2020-10-14 17:48:10 +0545

    added interactions to loginTest file

--- tests/loginTest.js
+++ tests/loginTest.js
@@ -14,20 +14,50 @@ fdescribe('Main: Currently testing Login and initLibrary,', function () {
   // PACT setup
   const Pact = require('@pact-foundation/pact-web')
   const provider = new Pact.PactWeb()
-  const { setGeneralInteractions } = require('./pactHelper.js')
-
-  beforeAll(function (done) {
-    Promise.all(setGeneralInteractions(provider)).then(done, done.fail)
-  })
-
-  afterAll(function (done) {
-    provider.removeInteractions().then(done, done.fail)
-  })
+  const { setGeneralInteractions, invalidAuthHeader } = require('./pactHelper.js')
 
   beforeEach(function () {
     oc = null
   })
-
+  beforeAll(function (done) {
+    const promises = []
+    promises.push(setGeneralInteractions(provider))
+    promises.push(provider.addInteraction({
+      uponReceiving: 'a capabilities GET request with invalid authentication',
+      withRequest: {
+        method: 'GET',
+        path: Pact.Matchers.term({
+          matcher: '.*\\/ocs\\/v(1|2)\\.php\\/cloud\\/capabilities',
+          generate: '/ocs/v1.php/cloud/capabilities'
+        }),
+        query: 'format=json',
+        headers: {
+          authorization: invalidAuthHeader,
+          Origin: origin
+        }
+      },
+      willRespondWith: {
+        status: 401,
+        headers: {
+          'Content-Type': 'application/json; charset=utf-8',
+          'Access-Control-Allow-Origin': origin
+        },
+        body: {
+          ocs: {
+            meta: {
+              status: 'failure',
+              statuscode: 997,
+              message: 'Unauthorised'
+            }
+          }
+        }
+      }
+    }))
+    Promise.all(promises).then(done, done.fail)
+  })
+  afterAll(function (done) {
+    provider.removeInteractions().then(done, done.fail)
+  })
   it('checking method : initLibrary to be null', function () {
     expect(oc).toBe(null)
   })
--- tests/pactHelper.js
+++ tests/pactHelper.js
@@ -153,37 +153,6 @@ function setGeneralInteractions (provider) {
     }
   }))
   promises.push(provider.addInteraction({
-    uponReceiving: 'a capabilities GET request with invalid authentication',
-    withRequest: {
-      method: 'GET',
-      path: Pact.Matchers.term({
-        matcher: '.*\\/ocs\\/v(1|2)\\.php\\/cloud\\/capabilities',
-        generate: '/ocs/v1.php/cloud/capabilities'
-      }),
-      query: 'format=json',
-      headers: {
-        authorization: invalidAuthHeader,
-        Origin: origin
-      }
-    },
-    willRespondWith: {
-      status: 401,
-      headers: {
-        'Content-Type': 'application/json; charset=utf-8',
-        'Access-Control-Allow-Origin': origin
-      },
-      body: {
-        ocs: {
-          meta: {
-            status: 'failure',
-            statuscode: 997,
-            message: 'Unauthorised'
-          }
-        }
-      }
-    }
-  }))
-  promises.push(provider.addInteraction({
     uponReceiving: 'a GET request to the cloud user endpoint',
     withRequest: {
       method: 'GET',

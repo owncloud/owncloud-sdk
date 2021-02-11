f3f3de90e0205468c264ab036c7528533f0c662b
jasson99 <jasmine.baral@hotmail.com>
2021-02-11 14:55:33 +0545

    provider tests for shareRecepient

--- tests/shareRecipientTest.js
+++ tests/shareRecipientTest.js
@@ -4,14 +4,21 @@ describe('Main: Currently testing share recipient,', function () {
   var config = require('./config/config.json')
 
   const {
-    validAdminAuthHeaders,
+    getAuthHeaders,
     getCapabilitiesInteraction,
     getCurrentUserInformationInteraction,
     createOwncloud,
     createProvider
   } = require('./pactHelper.js')
 
-  const getShareesInteraction = (provider) => {
+  const sharer = config.testUser
+  const receiver = config.testUser2
+
+  const getShareesInteraction = (provider, folder = config.testFolder) => {
+    provider
+      .given('the user is recreated', { username: sharer })
+      .given('the user is recreated', { username: receiver })
+      .given('group exists', { groupName: config.testGroup })
     return provider
       .uponReceiving('a request to get share recipients (both users and groups)')
       .withRequest({
@@ -21,7 +28,7 @@ describe('Main: Currently testing share recipient,', function () {
           '/ocs/v2.php/apps/files_sharing/api/v1/sharees'
         ),
         query: { search: 'test', itemType: 'folder', page: '1', perPage: '200', format: 'json' },
-        headers: validAdminAuthHeaders
+        headers: { authorization: getAuthHeaders(sharer, sharer) }
       }).willRespondWith({
         status: 200,
         headers: {
@@ -29,10 +36,10 @@ describe('Main: Currently testing share recipient,', function () {
         },
         body: {
           ocs: {
-            meta: { status: 'ok', statusCode: 200, message: 'OK', totalitems: '', itemsperpage: '' },
+            meta: { status: 'ok', statuscode: 200, message: 'OK', totalitems: '', itemsperpage: '' },
             data: {
               exact: { users: [], groups: [], remotes: [] },
-              users: [{ label: config.testUser, value: { shareType: 0, shareWith: config.testUser } }],
+              users: [{ label: sharer, value: { shareType: 0, shareWith: sharer } }, { label: receiver, value: { shareType: 0, shareWith: receiver } }],
               groups: [{ label: config.testGroup, value: { shareType: 1, shareWith: config.testGroup } }],
               remotes: []
             }
@@ -45,7 +52,7 @@ describe('Main: Currently testing share recipient,', function () {
     const provider = createProvider()
 
     return provider.executeTest(() => {
-      const oc = createOwncloud()
+      const oc = createOwncloud(sharer, sharer)
 
       return oc.shares.getRecipients('test', 'folder', 'a', 'b').then(status => {
         fail('share.getRecipients should have thrown an error.')
@@ -59,7 +66,7 @@ describe('Main: Currently testing share recipient,', function () {
     const provider = createProvider()
 
     return provider.executeTest(async () => {
-      const oc = createOwncloud()
+      const oc = createOwncloud(sharer, sharer)
 
       return oc.shares.getRecipients('test', 'folder', 2, 'b').then(status => {
         fail('share.getRecipients should have thrown an error.')
@@ -73,7 +80,7 @@ describe('Main: Currently testing share recipient,', function () {
     const provider = createProvider()
 
     return provider.executeTest(async () => {
-      const oc = createOwncloud()
+      const oc = createOwncloud(sharer, sharer)
 
       return oc.shares.getRecipients('test', 'folder', -1, 'b').then(status => {
         fail('share.getRecipients should have thrown an error.')
@@ -85,17 +92,20 @@ describe('Main: Currently testing share recipient,', function () {
 
   it('testing behavior : searching for users and groups', async function () {
     const provider = createProvider()
-    await getCapabilitiesInteraction(provider)
-    await getCurrentUserInformationInteraction(provider)
+    await getCapabilitiesInteraction(provider, sharer, sharer)
+    await getCurrentUserInformationInteraction(provider, sharer, sharer)
     await getShareesInteraction(provider)
 
     return provider.executeTest(async () => {
-      const oc = createOwncloud()
+      const oc = createOwncloud(sharer, sharer)
       await oc.login()
 
       return oc.shares.getRecipients('test', 'folder', 1, 200).then(resp => {
         expect(resp.users[0]).toMatchObject({
-          label: config.testUser
+          label: sharer
+        })
+        expect(resp.users[1]).toMatchObject({
+          label: receiver
         })
         expect(resp.groups[0]).toMatchObject({
           label: config.testGroup

547dc5ad22b32ae4a02be26aea1feb5df032ea7a
Pascal Wengerter <pascal@wengerter.info>
2022-07-04 14:51:24 +0200

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const ESLintPlugin = require('eslint-webpack-plugin')
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
@@ -15,16 +16,17 @@ module.exports = {
   },
   target: 'node', // in order to ignore built-in modules like path, fs, etc.
   externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
+  plugins: [
+    new ESLintPlugin({
+      useEslintrc: true
+    }
+    ),
+  ],
   module: {
     rules: [{
       test: /\.js?$/,
       exclude: /node_modules/,
       loader: 'babel-loader'
-    }, {
-      enforce: 'pre',
-      test: /\.(js|vue)$/,
-      exclude: /node_modules/,
-      loader: 'eslint-loader'
     }]
   }
 }
